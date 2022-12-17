b04929e66b0406a0ecdf0d976b4be3c7da7a2d53
ZarVeek <92723993+ZarVeek@users.noreply.github.com>
2022-12-17 19:52:35 +0500

    final

--- src/main/java/org/example/box/box.java
+++ src/main/java/org/example/box/box.java
@@ -45,7 +45,7 @@ public class box {
                     try {
                         System.out.println("Name: " + boxWithSweets.get(key).getName());
                         System.out.println("Id: " + boxWithSweets.get(key).getId());
-                        System.out.println("Weight: " + boxWithSweets.get(key).getPrice());
+                        System.out.println("Weight: " + boxWithSweets.get(key).getWeight());
                         System.out.println("Price: " + boxWithSweets.get(key).getPrice() + "\n");
                     } catch (NullPointerException e) {
                         System.out.println(e);
@@ -59,11 +59,11 @@ public class box {
             public void SmartRemoveSmallestWight(float firstWight) {
                 float newWight = 0;
                 for (int key : boxWithSweets.keySet()) {
-                    newWight += boxWithSweets.get(key).getPrice();
+                    newWight += boxWithSweets.get(key).getWeight();
                 }
                 Map<Integer, Float> wightMap = new HashMap<>();
                 for (int key : boxWithSweets.keySet()) {
-                    wightMap.put(key, (boxWithSweets.get(key).getPrice()));
+                    wightMap.put(key, (boxWithSweets.get(key).getWeight()));
                 }
                 while (newWight > firstWight) {
                     for (int keys : wightMap.keySet()) {
@@ -82,18 +82,18 @@ public class box {
             public void SmartRemoveSmallestPrice(float firstWight) {
                 float newWight = 0;
                 for (int key : boxWithSweets.keySet()) {
-                    newWight += boxWithSweets.get(key).getPrice();
+                    newWight += boxWithSweets.get(key).getWeight();
                 }
                 Map<Integer, Float> priceMap = new HashMap<>();
                 for (int key : boxWithSweets.keySet()) {
                     priceMap.put(key, (boxWithSweets.get(key).getPrice()));
                 }
                 while (newWight > firstWight) {
-                    for (int keys : priceMap.keySet()) {
+                    for (int key : priceMap.keySet()) {
                         float min = Collections.min(priceMap.values());
-                        if (priceMap.get(keys).equals(min)) {
-                            boxWithSweets.remove(keys);
-                            priceMap.remove(keys);
+                        if (priceMap.get(key).equals(min)) {
+                            boxWithSweets.remove(key);
+                            priceMap.remove(key);
                             newWight -= min;
                             break;
                         }
@@ -107,12 +107,14 @@ public class box {
         sweetBox.add(3, new Marmalade("marmalade", 50, 40, 3));
         sweetBox.add(4, new Cake("cake", 35, 50, 4));
         sweetBox.infoAboutAllItems();
-        sweetBox.delete(1);
-        sweetBox.infoAboutPriceOfBox();
         sweetBox.infoAboutPriceOfBox();
         sweetBox.infoAboutWeightOfBox();
-        sweetBox.SmartRemoveSmallestPrice(200);
+        sweetBox.SmartRemoveSmallestPrice(175);
         sweetBox.SmartRemoveSmallestWight(200);
         sweetBox.infoAboutPriceOfBox();
+        sweetBox.infoAboutWeightOfBox();
+        sweetBox.delete(1);
+        System.out.println(boxWithSweets.keySet());
+
     }
 }
