6e2cd90191d18d709410f94a10eb288da0f4df9c
ZarVeek <zarvee7@gmail.com>
2022-12-21 18:22:37 +0500

    Lesson2 v2

--- src/main/java/lesson2/App.java
+++ src/main/java/lesson2/App.java
@@ -20,38 +20,35 @@ public class App {
     }
 
     private static void go(String text) {
-        String[] words = text.split("\\s+|-|!|\\?|_|,|\\."); //не понимаю почему
+        String[] words = text.split("\\P{L}+");
+        System.out.println(Arrays.toString(words));
         Map<String, Integer> wordCountTable = new TreeMap<>();
-        float size = 0;
         int maxInt = 0;
         List<String> maxWord = new ArrayList<>();
 
         for (String word : words) {
-            if (!word.equals("")) {
-                String uniqueWord = word.toLowerCase();
-                if (wordCountTable.containsKey(uniqueWord)) {
-                    wordCountTable.replace(uniqueWord, wordCountTable.get(uniqueWord),
-                            wordCountTable.get(uniqueWord) + 1);
-                    if (maxInt < wordCountTable.get(uniqueWord)) {
-                        maxWord.clear();
-                        maxInt = wordCountTable.get(uniqueWord);
-                        maxWord.add(word);
-                    }
-                    if ((!maxWord.contains(word)) && maxInt == wordCountTable.get(uniqueWord)) {
-                        maxWord.add(word);
-                    }
-
-                } else {
-                    wordCountTable.put(uniqueWord, 1);
+            String uniqueWord = word.toLowerCase();
+            if (wordCountTable.containsKey(uniqueWord)) {
+                wordCountTable.replace(uniqueWord, wordCountTable.get(uniqueWord),
+                        wordCountTable.get(uniqueWord) + 1);
+                if (maxInt < wordCountTable.get(uniqueWord)) {
+                    maxWord.clear();
+                    maxInt = wordCountTable.get(uniqueWord);
+                    maxWord.add(word);
                 }
-                size++;
+                if ((!maxWord.contains(word)) && maxInt == wordCountTable.get(uniqueWord)) {
+                    maxWord.add(word);
+                }
+
+            } else {
+                wordCountTable.put(uniqueWord, 1);
             }
         }
         System.out.println("по алфавиту: " + wordCountTable);
         System.out.println("Слово, встречающееся максимальное число раз: " + maxWord);
         for (String key : wordCountTable.keySet()) {
             System.out.println(key + ": " + wordCountTable.get(key) + " | " + String.format("%.1f",
-                    wordCountTable.get(key) / size * 100) + "%");
+                    wordCountTable.get(key) / (float)words.length * 100) + "%");
         }
     }
 }
